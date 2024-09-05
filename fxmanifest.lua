b90736d2e1033bbab30a62759ccd59b8e0947bf9
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 12:00:18 +0000

    chore: bump manifest version to v0.8.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 
 name 'bcs_radialmenu'
 author 'baguscodestudio'
-version '2.0.0'
+version      '0.8.0'
 repository 'https://github.com/baguscodestudio/bcs_radialmenu'
 description 'A radial menu for FiveM'
 
